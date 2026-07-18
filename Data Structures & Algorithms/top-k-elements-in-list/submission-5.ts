a2d470833dfd95b2d1cbcb9489bf8c052598f70a
Shoxrux Tursunov (Kevin) <49744161+viminizer@users.noreply.github.com>
2026-07-18 19:22:31 +0900

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const map = new Map<number, number>();
+        const buckets = [];
+        for (let e of nums) {
+            let val = map.get(e) ?? 0;
+            map.set(e, ++val);
+        }
+
+        for (let [k, v] of map) {
+            buckets.push([k, v]);
+        }
+
+        const sorted = buckets.sort((a,b)=>b[1]-a[1]);
+        const sliced = sorted.slice(0,k);
+        return sliced.map(e=>e[0])
+    }
+}
